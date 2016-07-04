b9799f6bec3829701b274d67ba683dcaacc5bc0b
N. Justus <nicola.justus@mni.thm.de>
2016-07-04 10:47:01 +0200

    Create SimpleTest

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,11 @@
+
+import org.junit.*;
+import org.junit.Test;
+import org.junit.Assert.*;
+
+public class SimpleTest {
+	@Test
+	public void simpleTest() {
+		System.out.println("test case haha");
+	}
+}
