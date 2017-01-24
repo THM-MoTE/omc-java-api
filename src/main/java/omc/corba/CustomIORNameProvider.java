59c8f4c86a6b25d19b9944f632b9123c4f01e1e2
N. Justus <nicola.justus@mni.thm.de>
2017-01-24 12:53:47 +0100

    Add :copyright: header

--- src/main/java/omc/corba/CustomIORNameProvider.java
+++ src/main/java/omc/corba/CustomIORNameProvider.java
@@ -1,3 +1,19 @@
+/** Copyright (C) 2016 Nicola Justus <nicola.justus@mni.thm.de>
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  */
+
 package omc.corba;
 
 import java.nio.file.Files;
@@ -7,7 +23,7 @@ import java.util.Optional;
 
 /** Provider for custom/self-defined IOR name (the suffix).
  * If unique=true the IOR file will be made unique by appending a number to the given suffix.
- *  
+ *
  *  <p><b>Note:</b><br/>
  *  The check for uniqueness is triggered one's in the constructor-call.
  *  After instantiation the final IOR name is fixed!
@@ -18,7 +34,7 @@ public class CustomIORNameProvider implements IORNameProvider {
   private final boolean unique;
   private final IORNameProvider stdProvider = new StdIORNameProvider();
   private final Path iorPath;
-  
+
   public CustomIORNameProvider(String suffix, boolean unique) {
     this.unique = unique;
     this.suffix = createUniqueName(stdProvider.getPath(), "."+suffix);
