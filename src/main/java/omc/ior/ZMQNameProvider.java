9933cea6911ff7c8df5671f7a61045532326be51
N. Justus <nicola.justus@mni.thm.de>
2018-11-30 13:59:50 +0100

    add IORPRovider for ZMQ

---
+++ src/main/java/omc/ior/ZMQNameProvider.java
@@ -0,0 +1,19 @@
+package omc.ior;
+
+import omc.Global;
+
+import java.nio.file.Path;
+import java.util.Optional;
+
+public class ZMQNameProvider extends IORNameProvider {
+  public static final String DEFAULT_PORT = "om_local";
+  @Override
+  public Optional<String> getSuffix() {
+    return Optional.empty();
+  }
+
+  @Override
+  public Path getPath() {
+    return Global.tmpDir.resolve(String.format("openmodelica.%s.port.&s", Global.username, DEFAULT_PORT));
+  }
+}
